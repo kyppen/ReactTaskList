84d9818615c6fc2b64a7df943bc595c42716cc04
Sondre Fagerhus <Fagerhus.sondre@gmail.com>
2023-10-17 16:55:04 +0200

    removed outdates comments

--- server/TasksApi.js
+++ server/TasksApi.js
@@ -5,17 +5,13 @@ import dotenv from "dotenv";
 dotenv.config()
 
 
-
-
 export const TasksApi = express.Router();
 
-//const uri = "mongodb+srv://admin:i00oKsCDehVuQh1e@kyppencluster.trmuwta.mongodb.net/TaskList?retryWrites=true&w=majority";
 
 const mongoClient = new MongoClient(process.env.url)
 
 const data = await mongoClient.db().collection("Task").find({}).toArray();
 
-//const response = await mongoClient.db().collection("Task").insertOne({data: "something"})
 
 console.log(data)
 
@@ -35,32 +31,3 @@ mongoClient.connect().then((connection) => {
     })
 
 })
-
-
-const Tasks = [
-    {
-        id: 0,
-        taskName: "Create prject"
-    },{
-        id:1,
-        taskName: "add TypeScript"
-    },{
-        id:2,
-        taskName: "add Prettier"
-    }
-]
-/*
-TasksApi.get("", (req,res) => {
-    console.log("Somerthing happend")
-    res.json(Tasks)
-})
-
-TasksApi.post("",(req,res) => {
-    const {task} = req.body;
-    Tasks.push({
-        id: Tasks.length,
-        data: task
-    } )
-    console.log(Tasks)
-    res.status(200);
-})*/
